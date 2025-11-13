6b4dccfd5c47c0438684aa30c3f8cf9996474bb3
dixxew <ayrat.volodin2@gmail.com>
2025-11-13 21:00:56 +0300

    Updated stopwords Added StopWords to global top words

--- OsuRussianRep/Services/UserWordStatsService.cs
+++ OsuRussianRep/Services/UserWordStatsService.cs
@@ -14,14 +14,14 @@ public sealed class UserWordStatsService(AppDbContext db) : IUserWordStatsServic
 {
     private static readonly HashSet<string> StopWords = new(StringComparer.OrdinalIgnoreCase)
     {
-        "я", "ты", "он", "она", "мы", "вы", "они", "как", "это", "что", 
-        "в", "во", "на", "по", "из", "и", "или", "а", "но", "же", "то",
-        "к", "с", "у", "о", "об", "от", "до", "за", "для", "под", "там",
-        "osu", "pp", "https", "http", "twitch", "discord", "sh",  "ещё",
-        "com", "ru", "org", "net", "youtu", "youtube", "ss", "так", "да",
-        "www", "ppy", "osu.ppy", "osu.ppy.sh", "мне", "ну", "меня", "live", "65535", "io",
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "staticflickr", "jpg", "png", 
-        "если", "даже", "нибудь", "ща", "кто","зачем","где","нет","еще","есть","без","s","не"
+        "я", "ты", "он", "она", "мы", "вы", "они", "как", "это", "что", "бы", "вот",  "чё",  "че", 
+        "в", "во", "на", "по", "из", "и", "или", "а", "но", "же", "то", "щас", "всё", "когда", "уже", 
+        "к", "с", "у", "о", "об", "от", "до", "за", "для", "под", "там", "чтобы", "чтоб", "только", "тока",    
+        "osu", "pp", "https", "http", "twitch", "discord", "sh",  "ещё", "какойто", "какоето",    
+        "com", "ru", "org", "net", "youtu", "youtube", "ss", "так", "да", "какието", "тоже", "его", "их",       
+        "www", "ppy", "osu.ppy", "osu.ppy.sh", "мне", "ну", "меня", "live", "65535", "io",  "ul", "eu",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "staticflickr", "jpg", "png",  "beatmapsets",
+        "если", "даже", "нибудь", "ща", "кто","зачем","где","нет","еще","есть","без","s","не" 
     };
     
     public async Task<IReadOnlyList<TopWordDto>> GetTopWordsForUser(Guid userId, int limit, CancellationToken ct)
--- OsuRussianRep/Services/WordStatsService.cs
+++ OsuRussianRep/Services/WordStatsService.cs
@@ -13,12 +13,24 @@ public interface IWordStatsService
 
 public sealed class WordStatsService(AppDbContext db) : IWordStatsService
 {
+    private static readonly HashSet<string> StopWords = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "я", "ты", "он", "она", "мы", "вы", "они", "как", "это", "что", "бы", "вот",  "чё",  "че", 
+        "в", "во", "на", "по", "из", "и", "или", "а", "но", "же", "то", "щас", "всё", "когда", "уже", 
+        "к", "с", "у", "о", "об", "от", "до", "за", "для", "под", "там", "чтобы", "чтоб", "только", "тока",    
+        "osu", "pp", "https", "http", "twitch", "discord", "sh",  "ещё", "какойто", "какоето",    
+        "com", "ru", "org", "net", "youtu", "youtube", "ss", "так", "да", "какието", "тоже", "его", "их",       
+        "www", "ppy", "osu.ppy", "osu.ppy.sh", "мне", "ну", "меня", "live", "65535", "io",  "ul", "eu",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "staticflickr", "jpg", "png",  "beatmapsets",
+        "если", "даже", "нибудь", "ща", "кто","зачем","где","нет","еще","есть","без","s","не" 
+    };
+    
     public async Task<IReadOnlyList<TopWordDto>> GetTopWords(DateOnly from, DateOnly to, int limit, CancellationToken ct)
     {
         var capped = Math.Clamp(limit, 1, 500);
 
         var data = await db.WordsInDay.AsNoTracking()
-            .Where(wd => wd.Day >= from && wd.Day < to)
+            .Where(wd => wd.Day >= from && wd.Day < to && !StopWords.Contains(wd.Word.Lemma))
             .GroupBy(wd => wd.WordId)
             .Select(g => new { WordId = g.Key, Cnt = g.Sum(x => x.Cnt) })
             .OrderByDescending(x => x.Cnt)
